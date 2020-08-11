1c8d5b98c67160f2e6e9a15faf8b1d31df52d6ae
unknown <jweant@torchtechnologies.com>
2020-08-11 14:54:54 -0500

    Initial checkin

---
+++ src/main/java/log4j2/AnotherClass.java
@@ -0,0 +1,12 @@
+package log4j2;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class AnotherClass {
+	// Find or create a logger for a named subsystem. If a logger has already been created with the given name it is returned. Otherwise a new logger is created.
+	//private final static Logger LOGGER = LogManager.getLogger(Logger.GLOBAL_LOGGER_NAME);
+	// Create a local logger for ConfigBuilder: this is generally how I've seen loggers used....
+	//private final static Logger cbLogger = LogManager.getLogger(App.class.getName());
+	
+}
